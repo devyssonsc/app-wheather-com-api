4dc104f4e7e64bd3c80241d5d28a056c61f1dbaa
Devysson Cardoso <devyssonsc@gmail.com>
2023-11-05 00:02:30 +0000

    app complete

--- js/script.js
+++ js/script.js
@@ -1,1 +1,58 @@
-// 95efa740d2c65087ee8a19dfd32984c0
+//Variáveis e selaçãode elementos
+const apiKey = "95efa740d2c65087ee8a19dfd32984c0";
+const apiCountryURL = "https://flagsapi.com/"
+
+const cityInput = document.querySelector("#city-input");
+const searchBtn = document.querySelector("#search");
+
+const cityElement = document.querySelector("#city");
+const tempElement = document.querySelector("#temperature span");
+const descElement = document.querySelector("#description");
+const weatherIconElement = document.querySelector("#weather-icon");
+const countryElement = document.querySelector("#country");
+const humidityElement = document.querySelector("#humidity span");
+const windElement = document.querySelector("#wind span");
+
+const weatherContainer = document.querySelector("#weather-data");
+
+// Funções
+const getWeatherData = async(city) => {
+    const apiWeatherURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}&lang=pt_br`;
+
+    const res = await fetch(apiWeatherURL);
+    const data = await res.json();
+
+    return data
+}
+
+const showWeatherData = async (city) => {
+
+    const data = await getWeatherData(city);
+
+    cityElement.innerText = data.name;
+    tempElement.innerText = parseInt(data.main.temp);
+    descElement.innerText = data.weather[0].description;
+    weatherIconElement.setAttribute("src", `http://openweathermap.org/img/wn/${data.weather[0].icon}.png`)
+    countryElement.setAttribute("src", `${apiCountryURL}${data.sys.country}/flat/64.png`);
+    humidityElement.innerText = `${data.main.humidity}%`;
+    windElement.innerText = `${data.wind.speed} km/h`;
+
+    weatherContainer.classList.remove("hide");
+}
+
+// Eventos
+searchBtn.addEventListener("click", (e) => {
+    e.preventDefault();
+
+    const city = cityInput.value;
+
+    showWeatherData(city)
+})
+
+cityInput.addEventListener("keyup", (e) => {
+    if(e.code === "Enter"){
+        const city = e.target.value;
+
+        showWeatherData(city);
+    }
+})
